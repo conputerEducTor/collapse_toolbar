7b2189c61e3741084e7c55d8c0bd046be0ca9621
conputerEducTor <avhadnirmal072@gmail.com>
2021-04-23 17:24:17 +0530

    Collapsing Toolbar

---
+++ app/src/main/java/com/project/toolbar/ByFlight.java
@@ -0,0 +1,14 @@
+package com.project.toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ByFlight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_by_flight);
+    }
+}
